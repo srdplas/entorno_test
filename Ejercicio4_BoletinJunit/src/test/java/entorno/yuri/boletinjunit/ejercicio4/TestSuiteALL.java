ceeed330f534f51eaa503ad7e034a68a4e661b33
srdplas <cabrerozayas199@gmail.com>
2022-01-30 17:36:57 +0100

    Arreglado ERROR dependencia de Junit-Jupier_engine

--- Ejercicio4_BoletinJunit/src/test/java/entorno/yuri/boletinjunit/ejercicio4/TestSuiteALL.java
+++ Ejercicio4_BoletinJunit/src/test/java/entorno/yuri/boletinjunit/ejercicio4/TestSuiteALL.java
@@ -1,8 +1,8 @@
 package entorno.yuri.boletinjunit.ejercicio4;
 import org.junit.platform.suite.api.*;
 
-@SelectClasses( { BoaTest.class, FridgeTest.class, OperadorAritmeticoTest.class, PilaTest.class, SubscripcionTest.class } )
 @Suite
+@SelectClasses( { BoaTest.class, FridgeTest.class, OperadorAritmeticoTest.class, PilaTest.class, SubscripcionTest.class } )
 public class TestSuiteALL {
 
 }
